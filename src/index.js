467b0e0a4a90e851c3c62213d142d37e93f39f35
Yash Patel <86044919+Yash472001@users.noreply.github.com>
2021-07-01 16:42:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App';
 import './index.css'
-import {BrowserRouter} from "react-router-dom";
+import { HashRouter as Router} from "react-router-dom";
 
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById('root')
 );
 
